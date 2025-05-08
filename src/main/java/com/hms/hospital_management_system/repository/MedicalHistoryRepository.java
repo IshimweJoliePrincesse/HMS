fa137a343b4f867daf969f00c4a5bf944eff1939
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2025-05-08 21:53:23 +0200

    new changes

--- src/main/java/com/hms/hospital_management_system/repository/MedicalHistoryRepository.java
+++ src/main/java/com/hms/hospital_management_system/repository/MedicalHistoryRepository.java
@@ -4,4 +4,5 @@ import com.hms.hospital_management_system.entity.MedicalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory,Long > {
+
 }
---
+++ src/main/java/com/hms/hospital_management_system/security/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.hms.hospital_management_system.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    //inject dependencies
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
+
+        //get JWT (token) from http request
+        String token = getJWTfromRequest(request);
+
+        //validate token
+        if(StringUtils.hasText(token) && tokenProvider.validateToken(token)){
+
+        }
+    }
+}
